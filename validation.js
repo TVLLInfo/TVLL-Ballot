178e5b7d7ac9b77aed1dab56a7ed4ff65db748ae
TVLLInfo <information@temescalvalleybaseball.com>
2024-06-12 10:06:41 -0700

    Create validation.js

---
+++ validation.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>TVLL Ballot</title>
+  <script src="validation.js"></script>
+</head>
+<body>
+  <form onsubmit="return validateForm(this)">
+    </form>
+</body>
+</html>
